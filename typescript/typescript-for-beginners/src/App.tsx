bdbf46b45bbc84b4c1e846977ebf4838661c941b
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-08-15 13:06:42 +0900

    fin lec22 typescript for beginners

--- typescript/typescript-for-beginners/src/App.tsx
+++ typescript/typescript-for-beginners/src/App.tsx
@@ -24,6 +24,7 @@ import { Profile } from './components/auth/Profile';
 import { List } from './components/generics/List';
 import { RandomNumber } from './components/restriction/RandomNumber';
 import { Toast } from './components/templateliterals/Toast';
+import { CustomButton } from './components/html/Button';
 
 function App() {
   // const personName = {
@@ -95,7 +96,10 @@ function App() {
         onClick={(item) => console.log(item)}
       /> */}
       {/* <RandomNumber value={10} isPositive /> */}
-      <Toast position="center" />
+      {/* <Toast position="center" /> */}
+      <CustomButton variant="primary" onClick={() => console.log('clicked')}>
+        Primary Button
+      </CustomButton>
     </div>
   );
 }
