299ede857d8a122f6a2bc16f5d5e321b8e480cdf
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-08-17 22:52:57 +0900

    220817

--- typescript/typescript-blockchain/chapter8/dist/bc101_proof_of_work.js
+++ typescript/typescript-blockchain/chapter8/dist/bc101_proof_of_work.js
@@ -1,41 +1,47 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const crypto = require("crypto");
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const crypto = require('crypto');
 class Block {
-    constructor(index, previousHash, timestamp, data) {
-        this.index = index;
-        this.previousHash = previousHash;
-        this.timestamp = timestamp;
-        this.data = data;
-        const { nonce, hash } = this.mine(); // Calculates nonce and hash
-        this.nonce = nonce;
-        this.hash = hash;
-    }
-    calculateHash(nonce) {
-        const data = this.index + this.previousHash + this.timestamp + this.data + nonce; // Nonce is part of the input for calculating the hash
-        return crypto.createHash('sha256').update(data).digest('hex');
-    }
-    mine() {
-        let hash;
-        let nonce = 0;
-        do {
-            hash = this.calculateHash(nonce++); // Uses brute force for data mining
-        } while (hash.startsWith('00000') === false); // Runs this loop until the hash starts with 00000
-        return { nonce, hash };
-    }
+  constructor(index, previousHash, timestamp, data) {
+    this.index = index;
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+    this.data = data;
+    const { nonce, hash } = this.mine(); // Calculates nonce and hash
+    this.nonce = nonce;
+    this.hash = hash;
+  }
+  calculateHash(nonce) {
+    const data =
+      this.index + this.previousHash + this.timestamp + this.data + nonce; // Nonce is part of the input for calculating the hash
+    return crypto.createHash('sha256').update(data).digest('hex');
+  }
+  mine() {
+    let hash;
+    let nonce = 0;
+    do {
+      hash = this.calculateHash(nonce++); // Uses brute force for data mining
+    } while (hash.startsWith('00000') === false); // Runs this loop until the hash starts with 00000
+    return { nonce, hash };
+  }
 }
 class Blockchain {
-    constructor() {
-        this.chain = [];
-        this.chain.push(new Block(0, '0', Date.now(), 'Genesis block'));
-    }
-    get latestBlock() {
-        return this.chain[this.chain.length - 1];
-    }
-    addBlock(data) {
-        const block = new Block(this.latestBlock.index + 1, this.latestBlock.hash, Date.now(), data);
-        this.chain.push(block);
-    }
+  constructor() {
+    this.chain = [];
+    this.chain.push(new Block(0, '0', Date.now(), 'Genesis block'));
+  }
+  get latestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+  addBlock(data) {
+    const block = new Block(
+      this.latestBlock.index + 1,
+      this.latestBlock.hash,
+      Date.now(),
+      data
+    );
+    this.chain.push(block);
+  }
 }
 console.log('Creating the blockchain with the genesis block...');
 const blockchain = new Blockchain();
@@ -44,57 +50,3 @@ blockchain.addBlock('First block');
 console.log('Mining block #2...');
 blockchain.addBlock('Second block');
 console.log(JSON.stringify(blockchain, null, 2));
-// import * as crypto from 'crypto';
-// class Block {
-//   readonly nonce: number;
-//   readonly hash: string;
-//   constructor(
-//     readonly index: number,
-//     readonly previousHash: string,
-//     readonly timestamp: number,
-//     readonly data: string
-//   ) {
-//     const { nonce, hash } = this.mine();
-//     this.nonce = nonce;
-//     this.hash = hash;
-//   }
-//   private calculateHash(nonce: number): string {
-//     const data =
-//       this.index + this.previousHash + this.timestamp + this.data + nonce;
-//     return crypto.createHash('sha256').update(data).digest('hex');
-//   }
-//   private mine(): { nonce: number; hash: string } {
-//     let hash: string;
-//     let nonce = 0;
-//     do {
-//       hash = this.calculateHash(++nonce);
-//     } while (hash.startsWith('00000') === false);
-//     return { nonce, hash };
-//   }
-// }
-// class Blockchain {
-//   private readonly chain: Block[] = [];
-//   private get latestBlock(): Block {
-//     return this.chain[this.chain.length - 1];
-//   }
-//   constructor() {
-//     // Create the genesis block.
-//     this.chain.push(new Block(0, '0', Date.now(), 'Genesis block'));
-//   }
-//   addBlock(data: string): void {
-//     const block = new Block(
-//       this.latestBlock.index + 1,
-//       this.latestBlock.hash,
-//       Date.now(),
-//       data
-//     );
-//     this.chain.push(block);
-//   }
-// }
-// console.log('Creating the blockchain with the genesis block...');
-// const blockchain = new Blockchain();
-// console.log('Mining block #1...');
-// blockchain.addBlock('First block');
-// console.log('Mining block #2...');
-// blockchain.addBlock('Second block');
-// console.log(JSON.stringify(blockchain, null, 2));
