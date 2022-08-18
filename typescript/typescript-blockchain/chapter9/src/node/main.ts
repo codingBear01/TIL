0d058c78e2085c5a03d5da53ea3fa8ae7f1ab0fc
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-08-18 12:26:49 +0900

    fin 217

---
+++ typescript/typescript-blockchain/chapter9/src/node/main.ts
@@ -0,0 +1,24 @@
+import { Blockchain, Block } from '../lib/bc_transactions';
+
+(async function main(): Promise<void> {
+  console.log('? Initializing the blockchain creating the genesis block...');
+
+  const bc = new Blockchain(); // Create a new blockchain
+
+  await bc.createGenesisBlock(); // Creates the genesis block
+
+  // Creates a pending transaction
+  bc.createTransaction({ sender: 'Beckham', recipient: 'Vani', amount: 10 });
+  bc.createTransaction({ sender: 'Scholes', recipient: 'Rooney', amount: 20 });
+
+  // Creates a new block and adds it to the blockchain
+  await bc.minePendingTransactions();
+
+  bc.createTransaction({ sender: 'Pirlo', recipient: 'Toni', amount: 30 });
+  bc.createTransaction({ sender: 'Ki', recipient: 'Son', amount: 40 });
+
+  await bc.minePendingTransactions();
+
+  // Prints the content of the blockchain
+  console.log(JSON.stringify(bc, null, 2));
+});
