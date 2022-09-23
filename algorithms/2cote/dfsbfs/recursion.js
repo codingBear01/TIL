6cd78b5f063f48bd3b8b51a9035584213e667a19
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-09-23 18:30:29 +0900

    [create] recursion

--- algorithms/2cote/dfsbfs/recursion.js
+++ algorithms/2cote/dfsbfs/recursion.js
@@ -1,3 +1,12 @@
+let i = 0;
+
 function recursiveFunction() {
-  console.log('재귀 함수를 호출한다능');
+  if (i === 10) return;
+
+  console.log(`${i + 1}번째 재귀 함수를 호출한다능`);
+  i++;
+
+  recursiveFunction();
 }
+
+recursiveFunction();
