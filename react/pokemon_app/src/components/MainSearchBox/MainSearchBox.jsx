608d19f8bb85a4d52a02152e8741ad356037c4cc
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-09-29 06:11:39 +0900

    [create] poke_dex 관련 파일 추가

---
+++ react/pokemon_app/src/components/MainSearchBox/MainSearchBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as C from './style';
+
+const MainSearchBox = () => {
+  return (
+    <C.MainSearchBoxWrap>
+      <div>
+        <img
+          src="logo.jpg"
+          alt=""
+          style={{ width: '100px', height: '100px' }}
+        />
+      </div>
+      <div>MainSearchBox</div>
+      <div>MainSearchBox</div>
+    </C.MainSearchBoxWrap>
+  );
+};
+
+export default MainSearchBox;
