46354f8bc24cfe5dc31b836b81e1cf0e7e4ddd0f
myeongmo kang <jokingbear01@gmail.com>
2022-12-13 00:08:43 +0900

    [TypeScript] finish chapter2 in typescript programming #8

---
+++ typescript/typescript_programming/chapter-2/dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: a,
+};
+let d = c.apple * 4;
+console.log('a', a);
+console.log('b', b);
+console.log('c', c);
+console.log('d', d);
+//# sourceMappingURL=index.js.map
---
+++ typescript/typescript_programming/chapter-2/src/index.ts
@@ -0,0 +1,12 @@
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: a,
+};
+let d = c.apple * 4;
+
+console.log('a', a);
+console.log('b', b);
+console.log('c', c);
+console.log('d', d);
