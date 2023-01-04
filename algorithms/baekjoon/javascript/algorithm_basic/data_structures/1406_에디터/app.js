2da5e3f2d4d73a5030fc327bc036b43e51bdb611
codingBear01 <jokingbear01@gmail.com>
2023-01-04 13:32:03 +0900

    [Algorithms] 백준 알고리즘 기초 1/2 자료구조 1874 스택 수열 with JS #10

--- algorithms/baekjoon/javascript/algorithm_basic/data_structures/1406_에디터/app.js
+++ algorithms/baekjoon/javascript/algorithm_basic/data_structures/1406_에디터/app.js
@@ -2,47 +2,44 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const str = input
-    .splice(0, 1)[0]
-    .split('')
-    .filter((val) => val !== '\r');
+const str = input.splice(0, 1)[0].split('');
 
 const n = parseInt(input.splice(0, 1));
 
-const mySolution = (str, data) => {
-    let idx = str.length;
+// const mySolution = (str, data) => {
+//     let idx = str.length;
 
-    for (let d of data) {
-        const oper = d[0];
+//     for (let d of data) {
+//         const oper = d[0];
 
-        if (oper === 'P') {
-            const d1 = d[2];
+//         if (oper === 'P') {
+//             const d1 = d[2];
 
-            if (idx >= str.length) {
-                str.push(d1);
-            } else {
-                str.splice(idx, 0, d1);
-            }
+//             if (idx >= str.length) {
+//                 str.push(d1);
+//             } else {
+//                 str.splice(idx, 0, d1);
+//             }
 
-            idx++;
-        } else if (oper === 'L' && idx > 0) {
-            idx--;
-        } else if (oper === 'D' && idx < str.length) {
-            idx++;
-        } else if (oper === 'B' && idx > 0) {
-            str.splice(idx - 1, 1);
-            idx--;
-        }
-    }
+//             idx++;
+//         } else if (oper === 'L' && idx > 0) {
+//             idx--;
+//         } else if (oper === 'D' && idx < str.length) {
+//             idx++;
+//         } else if (oper === 'B' && idx > 0) {
+//             str.splice(idx - 1, 1);
+//             idx--;
+//         }
+//     }
 
-    return str.join('');
-};
+//     return str.join('');
+// };
 
-console.log('mySolution', mySolution(str, input));
+// console.log('mySolution', mySolution(str, input));
 
 const answer1 = (str, data) => {
-    let lStack = str;
-    let rStack = [];
+    const lStack = str;
+    const rStack = [];
 
     for (let d of data) {
         const oper = d[0];
@@ -61,4 +58,4 @@ const answer1 = (str, data) => {
     return lStack.concat(rStack.reverse()).join('');
 };
 
-console.log(answer1(str, input));
+console.log('answer1', answer1(str, input));
--- algorithms/baekjoon/javascript/algorithm_basic/data_structures/1874_스택_수열/app.js
+++ algorithms/baekjoon/javascript/algorithm_basic/data_structures/1874_스택_수열/app.js
@@ -4,145 +4,59 @@ let input = fs
     .readFileSync(filePath)
     .toString()
     .trim()
-    .split(' ')
+    .split('\n')
     .map((val) => parseInt(val));
 
-const [n, k] = input;
+const n = input.splice(0, 1);
 
-const mySolution = (n, k) => {
-    let nums = [];
-    const result = [];
-
-    for (let i = 1; i <= n; i++) nums.push(i);
-
-    let idx = k - 1;
-
-    while (nums.length) {
-        const left = [];
-        const right = [];
-
-        if (nums.length >= k) {
-            const popped = nums.splice(idx, 1)[0];
-            result.push(popped);
-
-            for (let i = idx; i < nums.length; i++) {
-                left.push(nums[i]);
-            }
-            for (let i = 0; i < idx; i++) {
-                right.push(nums[i]);
-            }
-
-            nums = left.concat(right);
-        } else {
-            const popped = nums.shift();
-            result.push(popped);
-
-            for (let num of nums) {
-                left.push(num);
-            }
-
-            nums = left;
-        }
-    }
-
-    return `<${result.map((val) => String(val)).join(', ')}>`;
-};
-
-console.log('mySolution', mySolution(n, k));
-
-const answer1 = (n, k) => {
-    const people = [];
-    for (let i = 1; i <= n; i++) people.push(i);
-    const result = [];
-
-    while (people.length) {
-        for (let i = 0; i < k - 1; i++) {
-            people.push(people.shift());
-        }
-        result.push(people.shift());
-    }
-
-    return `<${result.map((val) => String(val)).join(', ')}>`;
-};
-
-console.log('answer1', answer1(n, k));
-
-const answer2 = (n, k) => {
-    const people = [];
-    for (let i = 1; i <= n; i++) people.push(i);
-    const result = [];
-
-    let idx = 0;
-
-    for (let i = 0; i < n; i++) {
-        idx += k - 1;
-
-        if (idx >= people.length) {
-            idx %= people.length;
-        }
-
-        result.push(people.splice(idx, 1)[0]);
-    }
-
-    return `<${result.map((val) => String(val)).join(', ')}>`;
-};
-
-console.log('answer2', answer2(n, k));
-
-const answer3 = (n, k) => {
-    const queue = [...new Array(n)].map((item, index) => (item = index + 1));
+const answer1 = (n, data) => {
+    const stack = [];
     const result = [];
     let count = 1;
 
-    while (queue.length) {
-        const shifted = queue.shift();
+    for (let num of data) {
+        while (count <= num) {
+            stack.push(count);
+            result.push('+');
+            count++;
+        }
 
-        if (count % k === 0) {
-            result.push(shifted);
+        if (stack.pop() === num) {
+            result.push('-');
         } else {
-            queue.push(shifted);
-        }
-
-        count++;
-    }
-
-    return `<${result.map((val) => String(val)).join(', ')}>`;
-};
-
-console.log('answer3', answer3(n, k));
-
-const answer4 = (n, k) => {
-    const arr = Array.from({ length: n }, (v, i) => i + 1);
-    const result = [];
-
-    for (let i = 0; i < n; i++) {
-        for (let j = 1; j <= k; j++) {
-            if (j === k) {
-                result.push(arr.shift());
-            } else {
-                arr.push(arr.shift());
-            }
+            return 'NO';
         }
     }
 
-    return `<${result.map((val) => String(val)).join(', ')}>`;
+    return result.join('\n');
 };
 
-console.log('answer4', answer4(n, k));
+console.log('answer1', answer1(n, input));
 
-const answer5 = (n, k) => {
-    const arr = Array.from({ length: n }, (v, i) => i + 1);
-    const result = [];
+const answer2 = (n, data) => {
+    const [stack, result] = [[], []];
+    let [count, isPossible] = [1, true];
 
-    let cnt = 0;
+    for (let num of data) {
+        while (count <= num) {
+            stack.push(count);
+            result.push('+');
+            count++;
+        }
 
-    while (arr.length) {
-        cnt = (cnt + k - 1) % arr.length;
-        result.push(arr[cnt]);
-        arr.splice(cnt, 1);
+        if (stack.pop() === num) {
+            result.push('-');
+        } else {
+            isPossible = false;
+            break;
+        }
     }
 
-    return '<' + result.join(', ') + '>';
+    if (!isPossible) {
+        return 'NO';
+    } else {
+        return result.join('\n');
+    }
 };
 
-console.log('answer5', answer5(n, k));
+console.log('answer2', answer2(n, input));
